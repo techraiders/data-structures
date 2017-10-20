83167f8fcd9602eae29fbd7c111b8edf16f963e2
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-20 14:35:12 +0530

    Implements push and pop method of stack build from javascript string data type.

--- js/main.js
+++ js/main.js
@@ -5,11 +5,14 @@ var Stack = function  () {
 };
 
 Stack.prototype.push = function  (val) {
-  
+  this.storage = this.storage + val;
+  return this.storage.length;
 };
 
 Stack.prototype.pop = function  () {
-  
+  var lastCharacter = this.storage.charAt(this.storage.length - 1);
+  this.storage = this.storage.slice(0, -1);
+  return lastCharacter;
 };
 
 Stack.prototype.size = function  () {
@@ -19,3 +22,8 @@ Stack.prototype.size = function  () {
 var myWeeklyMenu = new Stack();
 
 myWeeklyMenu.push('RedBeans');
+
+var r = myWeeklyMenu.pop();
+console.log(r);
+
+console.log(myWeeklyMenu);

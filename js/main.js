2475189dd15f53177abfaad1cf9b502665f63dfd
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-20 15:07:20 +0530

    Bug fix

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ var Stack = function  () {
 };
 
 Stack.prototype.push = function  (val) {
-  this.storage = this.storage + val;
+  this.storage = this.storage + '***' + val;
   return this.storage.length;
 };
 
 Stack.prototype.pop = function  () {
-  var lastCharacter = this.storage.charAt(this.storage.length - 1);
-  this.storage = this.storage.slice(0, -1);
-  return lastCharacter;
+  // slice off the last item
+  var lastItem = this.storage.slice(this.storage.lastIndexOf('***') + 3);
+  // updating the storage stack without the last item
+  this.storage = this.storage.substring(0, this.storage.lastIndexOf('***'));
+  return lastItem;
 };
 
 Stack.prototype.size = function  () {
@@ -23,7 +25,9 @@ var myWeeklyMenu = new Stack();
 
 myWeeklyMenu.push('RedBeans');
 
-var r = myWeeklyMenu.pop();
-console.log(r);
+myWeeklyMenu.push('BuleBeans');
+
+console.log(myWeeklyMenu.pop());
+
 
 console.log(myWeeklyMenu);

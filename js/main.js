c93bcdce2d83df3054233578fa22337506f26355
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-20 14:09:33 +0530

    Uses string to implement Stack DataStructures.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,21 @@
-function LinkedList() {
-  var ll = this;
-  ll.head = null;
-  ll.tail = null;
-}
+'use strict';
 
-function Node(value, next, prev) {
-  var node = this;
-  node.value = value;
-  node.next = next;
-  node.prev = prev;
-}
-
-LinkedList.prototype.addToHead = function(value) {
-  var newNode = new Node(value, this.head, null);
-  if (this.head) this.head.prev = newNode;
-  else this.tail = newNode;
-  this.head = newNode;
+var Stack = function  () {
+  this.storage = '';
 };
 
-var ll = new LinkedList();
-
-/*ll.addToHead(100);
-ll.addToHead(200);
-ll.addToHead(300);*/
-
-LinkedList.prototype.addToTail = function(value) {
-  var ll = this;
-  var newNode = new Node(value, null, ll.tail);
-  if (ll.tail) ll.tail.next = newNode;
-  else ll.head = newNode;
-  ll.tail = newNode;
+Stack.prototype.push = function  (val) {
+  
 };
 
-var myLL = new LinkedList();
-myLL.addToTail(10);
-myLL.addToTail(20);
-myLL.addToTail(30);
+Stack.prototype.pop = function  () {
+  
+};
 
-console.log('Head value: ' + myLL.head.value);
-console.log('Next value :' + myLL.head.next.value);
-console.log('Tail value :' + myLL.tail.value);
+Stack.prototype.size = function  () {
+  
+};
+
+var myWeeklyMenu = new Stack();
+
+myWeeklyMenu.push('RedBeans');

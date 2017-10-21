e47b78eba83a4f74724911677dda3b53849dda1c
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-21 16:40:52 +0530

    Distinguishes significant and insignificant time complexity of loops, nested loops and their operations.

--- js/time-complexity.js
+++ js/time-complexity.js
@@ -5,3 +5,14 @@ arr.pop(); // O(1);
 1 + 3 // O(1)
 for (...) {} // O(n)
 arr.unshift(); // O(n)
+
+for (var i...) { O(n)
+	1 + 1; // O(1)
+}
+
+for (var i...) { O(n)
+	for (var j...) { O(n) | O(n) * O(n)
+		3 + 3; O(1) | O(1) + O(1)
+		5 + 6; O(1)
+	}
+} O(n^2) * O(2) // constant time is minimal in comparison of big input size, so ignored; final time complexity is: O(n^2)

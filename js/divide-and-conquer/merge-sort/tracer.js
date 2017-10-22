6de00a1b5c6efbf851f1a0632fdf12e54799f122
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-22 14:07:24 +0530

    Shows how sorted value reaches to the first function call and finally returned.

--- js/divide-and-conquer/merge-sort/tracer.js
+++ js/divide-and-conquer/merge-sort/tracer.js
@@ -1,3 +1,4 @@
+//O(n*logn)
 mergeSort(list)
 	base case: if list.length < 2, return
 	break the list into halves L & R
@@ -9,14 +10,14 @@ list [34, 83, 10, 9, 1, 4]
 /* CALL STACK */
 {1} base case: if list.length < 2, return // FALSE
 	 	break the list into halves L & R [34, 83, 10] [9, 1, 4]
-		Lsorted = mergeSort(L) [34, 83, 10] {2} -> [10, 34, 83]
-		Rsorted = mergeSort(R)
-		return merge(Lsorted, Rsorted)
+		Lsorted =  [10, 34, 83]
+		Rsorted = mergeSort(R) [9,1,4] -> [1, 4, 9]
+		return merge(Lsorted, Rsorted) -> [1 ,4,9, 10, 34, 83]
 
-{2} base case: if list.length < 2, return // FALSE
+/*{2} base case: if list.length < 2, return // FALSE
 		break the list into halves L & R [34] [83, 10]
 		Lsorted = mergeSort(L) [34]
 		Rsorted = mergeSort(R) [83, 10]
 		// similarly we pass R i.e [83, 10] and it comes back sorted as [10, 83] 
-		return merge(Lsorted, Rsorted) [34], [10, 83] -> [10, 34, 83]
-/* END OF CALL STACK */
+		return merge(Lsorted, Rsorted) [34], [10, 83] -> [10, 34, 83]*/
+/* END OF CALL STACK */ 

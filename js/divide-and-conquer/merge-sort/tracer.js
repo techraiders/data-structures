ef89e79aabaf5fc8a0dc3dd94e5bef4083a387c6
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-22 09:57:06 +0530

    Divides the left side of the array till length 1

---
+++ js/divide-and-conquer/merge-sort/tracer.js
@@ -0,0 +1,26 @@
+mergeSort(list)
+	base case: if list.length < 2, return
+	break the list into halves L & R
+	Lsorted = mergeSort(L)
+	Rsorted = mergeSort(R)
+	return merge(Lsorted, Rsorted)
+list [34, 83, 10, 9, 1, 4]
+
+/* CALL STACK */
+{1} base case: if list.length < 2, return // FALSE
+	 	break the list into halves L & R [34, 83, 10] [9, 1, 4]
+		Lsorted = mergeSort(L) [34, 83, 10] {2}
+		Rsorted = mergeSort(R)
+		return merge(Lsorted, Rsorted)
+
+{2} base case: if list.length < 2, return // FALSE
+		break the list into halves L & R [34] [83, 10]
+		Lsorted = mergeSort(L) [34] {3}
+		Rsorted = mergeSort(R)
+		return merge(Lsorted, Rsorted)
+{3} base case: if list.length < 2, return // TRUE
+		break the list into halves L & R
+		Lsorted = mergeSort(L)
+		Rsorted = mergeSort(R)
+		return merge(Lsorted, Rsorted)
+/* END OF CALL STACK */

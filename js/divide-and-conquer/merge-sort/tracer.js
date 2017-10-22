a73f1e10b3f7b68dcf16eff8f8e455e95910eb7b
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-22 12:30:57 +0530

    Rsorted is computed, similarly followed by Lsorted, then Lsorted and Rsorted passed down to merge methods, which returns the merged solution of sorted array to its caller method on Lsorted in previous method of call stack.

--- js/divide-and-conquer/merge-sort/tracer.js
+++ js/divide-and-conquer/merge-sort/tracer.js
@@ -9,18 +9,14 @@ list [34, 83, 10, 9, 1, 4]
 /* CALL STACK */
 {1} base case: if list.length < 2, return // FALSE
 	 	break the list into halves L & R [34, 83, 10] [9, 1, 4]
-		Lsorted = mergeSort(L) [34, 83, 10] {2}
+		Lsorted = mergeSort(L) [34, 83, 10] {2} -> [10, 34, 83]
 		Rsorted = mergeSort(R)
 		return merge(Lsorted, Rsorted)
 
 {2} base case: if list.length < 2, return // FALSE
 		break the list into halves L & R [34] [83, 10]
 		Lsorted = mergeSort(L) [34]
-		Rsorted = mergeSort(R) 
-		return merge(Lsorted, Rsorted)
-/*{3} base case: if list.length < 2, return // TRUE
-		break the list into halves L & R
-		Lsorted = mergeSort(L)
-		Rsorted = mergeSort(R)
-		return merge(Lsorted, Rsorted)*/
+		Rsorted = mergeSort(R) [83, 10]
+		// similarly we pass R i.e [83, 10] and it comes back sorted as [10, 83] 
+		return merge(Lsorted, Rsorted) [34], [10, 83] -> [10, 34, 83]
 /* END OF CALL STACK */

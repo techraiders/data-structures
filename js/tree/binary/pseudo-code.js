e240855bdd75b0a958a487562ed1ada302e9fbf5
Navneet Prakash <hello_navneet@hotmail.com>
2017-11-11 15:58:02 +0530

    Pseudo codes binary search tree.

---
+++ js/tree/binary/pseudo-code.js
@@ -0,0 +1,14 @@
+// Constructor
+  // value
+  // left
+  // right
+
+// Methods
+	// insert (value) goal: find its proper place
+			// if value < current
+			// if (left)	go left
+			// else insert
+	// if value > current
+			// if (right) go right
+			// else insert
+	// contains

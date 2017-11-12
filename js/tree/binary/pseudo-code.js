84825afbf53188ddc3a56b0f255d012e3f7d677d
Navneet Prakash <hello_navneet@hotmail.com>
2017-11-12 18:33:09 +0530

    Adds stack trace of binary tree algo, and creates binary tree.

--- js/tree/binary/pseudo-code.js
+++ js/tree/binary/pseudo-code.js
@@ -12,3 +12,42 @@
 			// if (right) go right
 			// else insert
 	// contains
+===
+bst = BST(11);
+{value: 11, left: null, right: null}
+
+bst.insert(15);
+{ value: 11, left: null,
+	right: {	value: 15, left: null, right: null}
+}
+
+bst.insert(7);
+{value: 11,
+	left: {value: 7, left: null, right: null},
+	right: {value: 15, left: null, right: null}
+}
+
+bst.insert(5);
+{value: 11,
+	left: {value: 7,left: {value: 5, left: null, right: null}, right: null},
+	right: {value: 15, left: null, right: null}
+}
+
+bst.insert(17);
+{value: 11,
+	left: {value: 7,left: {value: 5, left: null, right: null}, right: null},
+	right: {value: 15, left: null, right: {value: 17, left: null, right: null} }
+}
+
+/* STACK TRACE */
+// if value < current ( 15 < 11 ) i.e false
+	// if (left) go left
+	  // this.left.insert(value) // 15
+	// else insert
+
+// if value > current
+	// if (right) go right
+	// else insert i.e new tree with value
+	// add to current's right
+
+/* STACK TRACE */

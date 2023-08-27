1a4cae04c802ea7d581863c0da1bdf135b630334
Navneet Prakash <hello_navneet@hotmail.com>
2023-08-28 02:25:01 +0530

    Create anagram.js
    Determines if the second string is the anagram of the first.
    An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.

---
+++ js/problem-solving/anagram.js
@@ -0,0 +1,27 @@
+const validAnagram = (first, second) => {
+    if (first.length === second.length) {
+        const frequency = {};
+        for (let char of first) {
+            frequency[char] ? frequency[char]++ : frequency[char] = 1;
+        }
+
+        for (let char of second) {
+            if (frequency[char]) {
+                frequency[char]--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+};
+
+validAnagram(``, ``); // true
+validAnagram(`aaz`, `zza`); // false
+validAnagram(`anagram`, `nagaram`); // true
+validAnagram(`rat`, `car`); // false
+validAnagram(`awesome`, `awesom`); // false
+validAnagram(`qwerty`, `qeywrt`); // true
+validAnagram(`texttwisttime`, `timetwisttext`); // true

983337da1454fa9283f262940f58d4866250d73e
Navneet Prakash <hello_navneet@hotmail.com>
2023-08-28 20:56:07 +0530

    Create sliding-window.js

---
+++ js/problem-solving/sliding-window.js
@@ -0,0 +1,28 @@
+const maxSubArraySum = (arr, num) => {
+   // find the sub array of the given size whose sum of element is the greatest number.
+   if (arr.length >= num) {
+    
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        sum += arr[i];
+    }
+       
+    let currentSum = sum;       
+    for (let i = num; i < arr.length; i++) {
+        currentSum += arr[i] - arr[i-num];
+        sum = Math.max(sum, currentSum);
+    }
+    return currentSum;
+   } else {
+       return null;
+   }
+};
+
+maxSubArraySum([1,2,5,2,8,1,5],2);
+
+// [1,2]
+// [2,5]
+// [5,2]
+// [2,8]
+// [8,1]
+// [1,5]
